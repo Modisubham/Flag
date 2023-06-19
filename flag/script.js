8963cbc3f4f55a891a66898be14eb6c0324cea91
Modisubham <126549890+Modisubham@users.noreply.github.com>
2023-06-19 12:15:57 +0530

    Add files via upload

---
+++ flag/script.js
@@ -0,0 +1,32 @@
+function brazilFunc()
+{
+    var c="this is the flag of Brazil";
+    document.getElementById("brazil").innerHTML =c;
+}
+function outBrazil()
+{
+    var c="";
+    document.getElementById("brazil").innerHTML =c;
+}
+
+function indiaFunc()
+{
+    var c="this is the flag of India";
+    document.getElementById("india").innerHTML =c;
+}
+function outIndia()
+{
+    var c="";
+    document.getElementById("india").innerHTML =c;
+}
+
+function argFunc()
+{
+    var c="this is the flag of Argentina";
+    document.getElementById("arg").innerHTML =c;
+}
+function outArg()
+{
+    var c="";
+    document.getElementById("arg").innerHTML =c;
+}
